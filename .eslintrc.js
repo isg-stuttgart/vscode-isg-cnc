5cd1b8bbd2cda37c6d577f973fce41dda9dfe40c
Andreas Hafner <andreas.hafner@isg-stuttgart.de>
2021-06-01 12:32:35 +0200

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,4 +8,15 @@ module.exports = {
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
     ],
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+        "@typescript-eslint/class-name-casing": "warn",
+        "@typescript-eslint/semi": "warn",
+        "curly": "warn",
+        "eqeqeq": "warn",
+        "no-throw-literal": "warn",
+        "semi": "off"
+    }
 };
