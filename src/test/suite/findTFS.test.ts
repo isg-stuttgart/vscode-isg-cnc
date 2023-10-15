79687435ace884b3b11ea51e7681643c00a4d752
JurekSeverin <st177263@stud.uni-stuttgart.de>
2023-10-16 01:08:38 +0200

    collect more info of failign test and readd nom cache

--- src/test/suite/findTFS.test.ts
+++ src/test/suite/findTFS.test.ts
@@ -12,6 +12,10 @@ suite("Find TFS Test", () => {
         const document = activeTextEditor?.document;
         const selection = activeTextEditor?.selection;
         console.log("selection: " + JSON.stringify(selection));
+        console.log("Expected start: " + JSON.stringify(document?.positionAt(37)));
+        console.log("Actual start: " + JSON.stringify(selection?.start));
+        console.log("Expected end: " + JSON.stringify(document?.positionAt(40)));
+        console.log("Actual end: " + JSON.stringify(selection?.end));
         assert.deepStrictEqual(selection?.start, document?.positionAt(37));
         assert.deepStrictEqual(selection?.end, document?.positionAt(40));
         assert.strictEqual(getCurrentFileOffset(), 40);
