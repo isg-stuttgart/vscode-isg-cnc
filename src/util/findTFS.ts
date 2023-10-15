dd450a1405d6f3b500c7692b8df88d26c81a37f4
JurekSeverin <st177263@stud.uni-stuttgart.de>
2023-10-16 00:09:58 +0200

    Try to fix Find TFS Test

--- src/util/findTFS.ts
+++ src/util/findTFS.ts
@@ -28,7 +28,6 @@ export function findNextTFS(): boolean {
             const match = regExTechnology.exec(textToMatch);
             if (match !== null) {
                 const startoffset = match.index + document.offsetAt(startposition);
-                fileoffset.setCursorPosition(startoffset);
                 const startPos = document.positionAt(startoffset);
                 const endPos = document.positionAt(startoffset + match[0].length);
                 const range = new vscode.Range(startPos, endPos);
