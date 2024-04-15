46e89d7d041028700237bbca7384c05ec9b30e8a
JurekSeverin <st177263@stud.uni-stuttgart.de>
2024-04-15 19:56:52 +0200

    Test cases for completion matching prefix

--- src/test/res/isgCycles_test.nc
+++ src/test/res/isgCycles_test.nc
@@ -6,5 +6,5 @@ L CYCLE [NAME=SysCalibToolSettingProbe.ecy \
   @P14=-360 \
   @P15=-360-360
   ]
-L CYCLE [NAME=SysCalibToolSettingProbe.ecy @P6=0-9999999 @P13=0-99999 @P22=0-99999 @P14=-360 @P15=360 ]
-L CYCLE[NAME=
+L CYCLE [NAME=SysCalibToolSettingProbe.ecy @P6=0-9999999 @P13=0-99999 @P22=0-99999 @P14=-360 @P15=360     @P]
+L CYCLE [NAME=SysCalib
--- src/test/suite/server/completion.test.ts
+++ src/test/suite/server/completion.test.ts
@@ -33,13 +33,13 @@ suite('LS Snippet Completion', () => {
             ["L CYCLE [NAME=SysCalibBacklash1", "@P2", "@P33", "@P64"],
             []
         );
-
-
+        // both completions should not have the textEdit attribute because no prefix was given
+        assert.strictEqual(sysCalibBacklash1Required?.textEdit, undefined);
+        assert.strictEqual(sysCalibBacklash1All?.textEdit, undefined);
     });
     test('Cycle Completion inside of cycle call and no prefix', async () => {
-        const vscodeDoc = await vscode.workspace.openTextDocument(testFileUri);
-        const doc = TextDocument.create(vscodeDoc.uri.toString(), "nc", 0, vscodeDoc.getText());
-        // completion request inside but before parameters would come should return no completions
+        const doc = await openCycleNcTestFile();
+        // completion request inside but before parameters should return no completions
         const completionsExpectedEmptyMultiLine = getCompletions(new vscode.Position(1, 13), doc);
         assert.strictEqual(completionsExpectedEmptyMultiLine.length, 0);
         const completionsExpectedEmptySingleLine = getCompletions(new vscode.Position(8, 13), doc);
@@ -60,12 +60,53 @@ suite('LS Snippet Completion', () => {
             // should not contain P14 because it is already in the cycle call
             assert.ok(!completions.some(c => c.label.toString().includes("P14")));
         }
+        // no completions should have the textEdit attribute because no prefix was given
+        completionsMulti.forEach(c => assert.strictEqual(c.textEdit, undefined));
+        completionsSingle.forEach(c => assert.strictEqual(c.textEdit, undefined));
     });
 
     test('Cycle Completion outside of cycle call and with prefix', async () => {
+        const doc = await openCycleNcTestFile();
+        // completion should replace the matched prefix
+        const completions = getCompletions(new vscode.Position(9, 22), doc);
+        const sysCalibBacklash1Completion = completions.find(c => c.label.toString().includes("SysCalibBacklash1") && c.label.toString().includes("all params"));
+        assert.ok(sysCalibBacklash1Completion);
+        // completion should have the textEdit attribute because a matching prefix was given
+        const textEdit = sysCalibBacklash1Completion?.textEdit as vscode.TextEdit;
+        assert.ok(textEdit);
+        assert.ok(textEdit?.newText.startsWith("L CYCLE [NAME=SysCalibBacklash1"));
+        assert.strictEqual(textEdit.range.start.line, 9);
+        assert.strictEqual(textEdit.range.start.character, 0);
+        assert.strictEqual(textEdit.range.end.line, 9);
+        assert.strictEqual(textEdit.range.end.character, 22);
+    });
 
+    test('Cycle Completion inside of cycle call and with prefix', async () => {
+        const doc = await openCycleNcTestFile();
+        // completion should replace the matched prefix
+        const completions = getCompletions(new vscode.Position(8, 108), doc);
+        const p16Completion = completions.find(c => c.label.toString().includes("P16"));
+        assert.ok(p16Completion);
+        // completion should have the textEdit attribute because a matching prefix was given
+        const textEdit = p16Completion?.textEdit as vscode.TextEdit;
+        assert.ok(textEdit);
+        assert.ok(textEdit?.newText.startsWith("@P16"));
+        assert.strictEqual(textEdit.range.start.line, 8);
+        assert.strictEqual(textEdit.range.start.character, 106);
+        assert.strictEqual(textEdit.range.end.line, 8);
+        assert.strictEqual(textEdit.range.end.character, 108);
     });
 });
+
+/**
+ * Opens the test file for cycle completion and returns the document.
+ * @returns the opened document as a Promise<TextDocument>
+ */
+async function openCycleNcTestFile(): Promise<TextDocument> {
+    const vscodeDoc = await vscode.workspace.openTextDocument(testFileUri);
+    const doc = TextDocument.create(vscodeDoc.uri.toString(), "nc", 0, vscodeDoc.getText());
+    return doc;
+}
 
 function getStringsOfAttributes(att: (string | undefined | vscode.SnippetString)[]): string[] {
     return att.map(a => {
