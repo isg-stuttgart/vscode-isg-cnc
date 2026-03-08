eb89083d12fdc039119f500918cdfbd180172862
philipptran-isg <philipptran-isg@users.noreply.github.com>
2026-03-08 22:17:58 +0000

    develop Synchronize from extension-ressources repository - Triggered by Github Workflows

--- server/extension-resources-output/src/JsonEntry.ts
+++ server/extension-resources-output/src/JsonEntry.ts
@@ -8,7 +8,7 @@ export interface JsonEntryUncompleted {
   label: string;
   linkid: string | null | undefined;
   shortDescription: Dict | null | undefined;
-  sublinks: { label: Dict; linkid: string | null }[] | null | undefined;
+  sublinks: { label: Dict | null; linkid: string | null }[] | null | undefined;
   kind: ItemKind | null | undefined;
   completionText: string | null | undefined;
   hoverText: Dict | null | undefined;
@@ -19,7 +19,7 @@ export class JsonEntry implements JsonEntryUncompleted {
   label: string;
   linkid: string | null;
   shortDescription: Dict;
-  sublinks: { label: Dict; linkid: string | null }[];
+  sublinks: { label: Dict | null; linkid: string | null }[];
   kind: ItemKind;
   completionText: string;
   filterText: string | null;
@@ -41,7 +41,7 @@ export class JsonEntry implements JsonEntryUncompleted {
     label: string,
     linkid: string | null,
     shortDescription: Dict,
-    sublinks: { label: Dict; linkid: string | null }[],
+    sublinks: { label: Dict | null; linkid: string | null }[],
     kind: ItemKind,
     completionText: string,
     hoverText: Dict,
@@ -69,8 +69,17 @@ export class JsonEntry implements JsonEntryUncompleted {
     const documentationLocalized = locale === Locale.de ? "Dokumentation" : "Documentation";
     const sublinksLocalized = locale === Locale.de ? "Verwandte Links:" : "Related Links:";
     const mainLink = linkid ? `\n\n[**${documentationLocalized}**](${docuPath}/${locale}/index.html#${linkid})` : '';
-    const sublinks = this.sublinks && this.sublinks.length > 0 ? (`\n\n**${sublinksLocalized}**\n` + this.sublinks.map(sublink => `- [${sublink.label[locale]}](${docuPath}/${locale}/index.html#${sublink.linkid})`).join('\n')) : '';
-    return `${infoText}${mainLink}${sublinks}`;
+    let sublinksString = "";
+    if (this.sublinks && this.sublinks.length > 0) {
+      const sublinkLocalized = locale === Locale.de ? "Hilfslink" : "Helper Link"; 
+      sublinksString = `\n\n**${sublinksLocalized}**\n`
+      for (let i = 0; i < this.sublinks.length; i++) {
+        const sublink = this.sublinks[i];
+        const label = sublink.label ? sublink.label[locale] : `${sublinkLocalized} ${i + 1}`;
+        sublinksString += `- [${label}](${docuPath}/${locale}/index.html#${sublink.linkid})\n`;
+      }
+    }
+    return `${infoText}${mainLink}${sublinksString}`;
   }
 
   getInfoTextWithVscodeCommand(getCommandUriToOpenDocu: (id: string) => string, locale: Locale): string {
@@ -79,8 +88,17 @@ export class JsonEntry implements JsonEntryUncompleted {
     const documentationLocalized = locale === Locale.de ? "Dokumentation" : "Documentation";
     const sublinksLocalized = locale === Locale.de ? "Verwandte Links:" : "Related Links:";
     const mainLink = linkid ? `\n\n[**${documentationLocalized}**](${getCommandUriToOpenDocu(linkid)})` : '';
-    const sublinks = this.sublinks && this.sublinks.length > 0 ? (`\n\n**${sublinksLocalized}**\n` + this.sublinks.map(sublink => `- [${sublink.label[locale]}](${getCommandUriToOpenDocu(sublink.linkid!)})`).join('\n')) : '';
-    return `${infoText[locale]}${mainLink}${sublinks}`;
+    let sublinksString = "";
+    if (this.sublinks && this.sublinks.length > 0) {
+      const sublinkLocalized = locale === Locale.de ? "Hilfslink" : "Helper Link";
+      sublinksString = `\n\n**${sublinksLocalized}**\n`
+      for (let i = 0; i < this.sublinks.length; i++) {
+        const sublink = this.sublinks[i];
+        const label = sublink.label ? sublink.label[locale] : `${sublinkLocalized} ${i + 1}`;
+        sublinksString += `- [${label}](${getCommandUriToOpenDocu(sublink.linkid!)})\n`;
+      }
+    }
+    return `${infoText[locale]}${mainLink}${sublinksString}`;
   }
 
   /** Parses a JSON object into a JsonEntry object and throws an error if the JSON is invalid */
@@ -184,7 +202,7 @@ export class JsonEntry implements JsonEntryUncompleted {
     // merge sublinks into current sublinks
     if (newObj.sublinks && Array.isArray(newObj.sublinks)) {
       newObj.sublinks.forEach((sublink: { label: Dict, linkid: string | null }) => {
-        const index = this.sublinks.findIndex((s) => s.label.equals(sublink.label));
+        const index = this.sublinks.findIndex((s) => s.label?.equals(sublink.label));
         if (index === -1) {
           this.sublinks.push(sublink);
         } else {
